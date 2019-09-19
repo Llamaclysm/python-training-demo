76a4f1da653b70408a459c65dbfcbeb5bd02665e
Llamaclysm <t.wycinka+github@gmail.com>
2019-09-19 12:30:54 +0200

    cleanup

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,6 +1,5 @@
-
 character_name = "Tommy"
-character_age = "100"
+character_age = "50"
 print("There once was a man named " + character_name + ", ")
 print("he was " + character_age + " years old. ")
 print("He really liked the name " + character_name + ", ")
