5d469e544c6c6669034e8e8021628535e7028f96
Llamaclysm <t.wycinka+github@gmail.com>
2019-09-19 12:44:25 +0200

    strings...

---
+++ strings.py
@@ -0,0 +1,37 @@
+print("- - - - - - - - - - - - - - -")
+print("Giraffe Academy")
+print("- - - - - - - - - - - - - - -")
+print("Giraffe\nAcademy")
+print("- - - - - - - - - - - - - - -")
+print("Giraffe\"Academy")
+print("- - - - - - - - - - - - - - -")
+phrase = "Giraffe Academy"
+print(phrase)
+print("- - - - - - - - - - - - - - -")
+phrase = "Giraffe Academy"
+print(phrase + " is cool")
+print("- - - - - - - - - - - - - - -")
+phrase = "Giraffe Academy"
+print(phrase.lower())
+print("- - - - - - - - - - - - - - -")
+phrase = "Giraffe Academy"
+print(phrase.upper())
+print("- - - - - - - - - - - - - - -")
+phrase = "Giraffe Academy"
+print(phrase.isupper())
+print("- - - - - - - - - - - - - - -")
+phrase = "Giraffe Academy"
+print(phrase.upper().isupper())
+print("- - - - - - - - - - - - - - -")
+phrase = "Giraffe Academy"
+print(len(phrase))
+print("- - - - - - - - - - - - - - -")
+phrase = "Giraffe Academy"
+print(phrase[8])
+print("- - - - - - - - - - - - - - -")
+phrase = "Giraffe Academy"
+print(phrase.index("A"))
+print("- - - - - - - - - - - - - - -")
+phrase = "Giraffe Academy"
+print(phrase.replace("Giraffe", "Zebra"))
+print("- - - - - - - - - - - - - - -")
